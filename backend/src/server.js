5d1c19b4d2c89ad2236b977d1f5c1fb23e2f71ba
Faizal <faizalamaan2@gmail.com>
2025-09-06 18:05:29 +0530

    database connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 
 app.use("/api/auth", authRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on PORT ${process.env.PORT} ✅`);
-});
+const PORT = process.env.PORT || 5000;
+const MONGOURL = process.env.MONGO_URI;
+const startServer = async () => {
+  try {
+    await mongoose.connect(MONGOURL); // Connect to MongoDB
+    console.log("✅ Database Connected Successfully");
+
+    const server = app.listen(PORT, () => {
+      console.log(`🚀 Server is Running on Port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Error connecting to the database:", error);
+  }
+};
+
+// Start the server
+startServer();
 
